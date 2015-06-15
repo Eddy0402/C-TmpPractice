fbde7edc3168cd2befb3f6d320171b7b62f9da64
ItsEddy <itseddy0402@gmail.com>
2015-06-15 13:16:00 +0800

    Add non-scaled user-defined-suffix

--- src/Unit.hpp
+++ src/Unit.hpp
@@ -15,6 +15,7 @@ class Scale_k;
 class Scale_M;
 class Scale_G;
 class Scale_T;
+class Scale_;
 class Scale_m;
 class Scale_u;
 class Scale_p;
@@ -24,6 +25,7 @@ typedef boost::mpl::map<boost::mpl::pair<Scale_k, boost::mpl::int_<3>>,
                         boost::mpl::pair<Scale_M, boost::mpl::int_<6>>,
                         boost::mpl::pair<Scale_G, boost::mpl::int_<9>>,
                         boost::mpl::pair<Scale_T, boost::mpl::int_<12>>,
+                        boost::mpl::pair<Scale_, boost::mpl::int_<0>>,
                         boost::mpl::pair<Scale_m, boost::mpl::int_<-3>>,
                         boost::mpl::pair<Scale_u, boost::mpl::int_<-6>>,
                         boost::mpl::pair<Scale_p, boost::mpl::int_<-9>>,
@@ -35,8 +37,9 @@ struct GenUnit_impl {
     typedef typename boost::mpl::at<ScaleConversion, Scale>::type ScaleIntType;
     typedef typename boost::units::make_scaled_unit<
         BaseUnit,
-        boost::units::
-            scale<10, boost::units::static_rational<ScaleIntType::value - ScaleToOriginal>>>::type
+        boost::units::scale<10,
+                            boost::units::static_rational<
+                                ScaleIntType::value - ScaleToOriginal>>>::type
         type;
 };
 template <class Scale, class BaseUnit, int ScaleToOriginal>
@@ -76,6 +79,15 @@ using GenUnit = typename GenUnit_impl<Scale, BaseUnit, ScaleToOriginal>::type;
                     SCALE_TO_ORIGINAL>>::from_value(in);                 \
     }                                                                    \
                                                                          \
+    boost::units::quantity<GenUnit<Scale_, boost::units::si::BASE_UNIT,  \
+                                   SCALE_TO_ORIGINAL>> inline            \
+    operator"" _##BASE_UNIT_SUFFIX(long double in)                       \
+    {                                                                    \
+        return boost::units::quantity<                                   \
+            GenUnit<Scale_, boost::units::si::BASE_UNIT,                 \
+                    SCALE_TO_ORIGINAL>>::from_value(in);                 \
+    }                                                                    \
+                                                                         \
     boost::units::quantity<GenUnit<Scale_m, boost::units::si::BASE_UNIT, \
                                    SCALE_TO_ORIGINAL>> inline            \
     operator"" _##m##BASE_UNIT_SUFFIX(long double in)                    \
@@ -110,6 +122,6 @@ using GenUnit = typename GenUnit_impl<Scale, BaseUnit, ScaleToOriginal>::type;
     }
 MAKE(length, m, 0)
 MAKE(mass, g, 3)
-MAKE(time, s, 3)
+MAKE(time, s, 0)
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,34 @@
 #include <utility>
 #include <typeinfo>
 #include <boost/mpl/list.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/equal.hpp>
 #include <boost/mpl/push_front.hpp>
+#include <boost/mpl/insert.hpp>
+#include <boost/mpl/begin.hpp>
 
 #include "MetaFunctions.hpp"
 void SwapTest();
 
 #include "Unit.hpp"
 
-using namespace boost::mpl;
+namespace mpl = boost::mpl;
+
+using before = boost::mpl::vector<int, float>;
+using after =
+    boost::mpl::insert<before, boost::mpl::begin<before>::type, double>::type;
 
 int main()
 {
+    static_assert(boost::mpl::equal<mpl::vector<double, int, float>, after>::type::value, "Not equal");
+
     auto m = 1.0_kg;
     std::cout << m << std::endl;
     auto l = 1.0_km;
     std::cout << l << std::endl;
-    auto t = 1.0_ks;
+    auto t = 1.0_ms;
     std::cout << t << std::endl;
-    std::cout << m * l * l/ t / t << std::endl;
+    std::cout << m* l* l / t / t << std::endl;
     std::cout << 1.0_kg * 1.0_mm * 1.0_km / 1.0_ms / 1.0_ms << std::endl;
-//    std::cout << 1.0_kg + 1.0_mm << std::endl;
+    //    std::cout << 1.0_kg + 1.0_mm << std::endl;
 }
