af7bc7ff40337ef827237dc6f647ff9b97b225af
ItsEddy <itseddy0402@gmail.com>
2015-06-16 17:00:54 +0800

    Revisit syntax

--- src/Unit.hpp
+++ src/Unit.hpp
@@ -34,38 +34,36 @@ typedef boost::mpl::map<boost::mpl::pair<Scale_k, boost::mpl::int_<3>>,
 
 template <class Scale, class BaseUnit, int ScaleToOriginal>
 struct GenUnit_impl {
-    typedef typename boost::mpl::at<ScaleConversion, Scale>::type ScaleIntType;
-    typedef typename boost::units::make_scaled_unit<
+    using ScaleIntType = typename boost::mpl::at<ScaleConversion, Scale>::type;
+    using type = typename boost::units::make_scaled_unit<
         BaseUnit,
         boost::units::scale<10,
                             boost::units::static_rational<
-                                ScaleIntType::value - ScaleToOriginal>>>::type
-        type;
+                                ScaleIntType::value - ScaleToOriginal>>>::type;
 };
 template <class Scale, class BaseUnit, int ScaleToOriginal>
 using GenUnit = typename GenUnit_impl<Scale, BaseUnit, ScaleToOriginal>::type;
 
-#define MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, SCALE, SCALE_TO_ORIGINAL)             \
+#define MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, SCALE, SCALE_TO_ORIGINAL)        \
     boost::units::quantity<GenUnit<Scale_##SCALE, boost::units::si::BASE_UNIT, \
-                                   SCALE_TO_ORIGINAL>> inline            \
-    operator"" _##SCALE##BASE_UNIT_SUFFIX(long double in)                    \
-    {                                                                    \
-        return boost::units::quantity<                                   \
+                                   SCALE_TO_ORIGINAL>> inline                  \
+    operator"" _##SCALE##BASE_UNIT_SUFFIX(long double in)                      \
+    {                                                                          \
+        return boost::units::quantity<                                         \
             GenUnit<Scale_##SCALE, boost::units::si::BASE_UNIT,                \
-                    SCALE_TO_ORIGINAL>>::from_value(in);                 \
-    }                                                                    \
+                    SCALE_TO_ORIGINAL>>::from_value(in);                       \
+    }
 
-#define MAKE(BASE_UNIT, BASE_UNIT_SUFFIX, SCALE_TO_ORIGINAL)             \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, k, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, M, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, G, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, T, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX,  , SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, m, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, u, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, p, SCALE_TO_ORIGINAL)          \
-    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, f, SCALE_TO_ORIGINAL)          \
-                                                                         \
+#define MAKE(BASE_UNIT, BASE_UNIT_SUFFIX, SCALE_TO_ORIGINAL)    \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, k, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, M, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, G, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, T, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, , SCALE_TO_ORIGINAL)  \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, m, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, u, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, p, SCALE_TO_ORIGINAL) \
+    MAKE_ONE(BASE_UNIT, BASE_UNIT_SUFFIX, f, SCALE_TO_ORIGINAL)
 
 MAKE(length, m, 0)
 MAKE(mass, g, 3)
